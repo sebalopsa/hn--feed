62920280f4781cae98f3748cc79c22184e76ed4d
Sebastian Lopez <spls90@gmail.com>
2022-09-06 11:06:40 -0300

    dockerize artifacts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,7 @@ import { StoriesModule } from './stories/stories.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/hn-feed'),
+    MongooseModule.forRoot('mongodb://db:27017/hn-feed'),
     StoriesModule,
   ],
 })
